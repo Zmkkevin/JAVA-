d3fbf097246351708f3fbd5fb36f7b6bd9d68978
kevin Zhang <49096554+Zmkkevin@users.noreply.github.com>
2021-07-14 23:00:55 +0800

    Add files via upload

---
+++ 牛客华为题库/T16.java
@@ -0,0 +1,9 @@
+package Niuke;
+
+public class T16 {
+    public static void main(String[] args) {
+        String s = "A10";
+        int num = Integer.valueOf(s.substring(1));
+        System.out.println(num);
+    }
+}
